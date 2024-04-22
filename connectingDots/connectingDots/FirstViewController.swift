a32bf3bccccd17764b0974ff8e04847ba099c9a6
Nayeon Chae <cny1230@naver.com>
2024-04-22 23:06:39 +0900

    finished

--- connectingDots/connectingDots/FirstViewController.swift
+++ connectingDots/connectingDots/FirstViewController.swift
@@ -1,14 +1,27 @@
-
-
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var count: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBAction func incrementButton(_ sender: Any) {
+        count += 1
+        countLabel.text = "\(count)"
     }
-
-
+    
+    @IBAction func decrementButton(_ sender: Any) {
+        count -= 1
+        countLabel.text = "\(count)"
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        if let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController {
+            secondVC.receivedCount = self.count
+            self.present(secondVC, animated: true, completion: nil) } else {
+                // Handle the case where the view controller instantiation fails
+                print("Failed to instantiate SecondViewController")
+            }
+    }
 }
-
--- connectingDots/connectingDots/SecondViewController.swift
+++ connectingDots/connectingDots/SecondViewController.swift
@@ -1,14 +1,14 @@
 
-
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    var receivedCount: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+        countLabel.text = "\(receivedCount)"
     }
-    
-
 }
